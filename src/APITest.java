9fa162606c7bdc9c397a5d96396e71979d41292e
TheCaister <edisoncai2001@gmail.com>
2022-06-02 14:10:08 +0200

    Trying to build full responses

--- src/APITest.java
+++ src/APITest.java
@@ -1,10 +1,8 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class APITest {
     public static void main(String[] args) throws IOException {
@@ -19,11 +17,89 @@ public class APITest {
         con.setRequestMethod("GET");
         con.setDoOutput(true);
 
+        // Adding headers
+        con.setRequestProperty("Content-Type", "application/json");
+
+        // Reading headers
+        String contentType = con.getHeaderField("Content-Type");
+
+        // Setting timeouts
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+
+        // Working with cookies
+        String cookiesHeader = con.getHeaderField("Set-Cookie");
+        // Getting list of cookies
+        List<HttpCookie> cookies = HttpCookie.parse(cookiesHeader);
+
+        CookieManager cookieManager = null;
+
+        // Perform this function to each cookie in the list
+        cookies.forEach(cookie -> cookieManager.getCookieStore().add(null, cookie));
+
+        // Get the username cookie. Optional makes it so that it can be empty.
+        Optional<HttpCookie> usernameCookie = cookies.stream().findAny().filter(
+                cookie -> cookie.getName().equals("username")
+        );
+
+        // If no username cookie exists, add one.
+        if (usernameCookie == null) {
+            cookieManager.getCookieStore().add(null, new HttpCookie("username", "ed"));
+        }
+
+        // To add cookies to the request, the Cookie header must be set.
+        // Connection must be closed and reopened.
+        con.disconnect();
+        con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestProperty("Cookie",
+                StringUtils.join(cookieManager.getCookieStore().getCookies(), ";"));
+
+        // Redirects - Can set to either true or false
+        //con.setInstanceFollowRedirects(false);
+        //HttpURLConnection.setFollowRedirects(false);
+
+        // If a request returns a status code 301 or 302 (indicating a redirect),
+        // retrieve the Location header and create a new request to the new URL.
+        int status = 0;
+        if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                || status == HttpURLConnection.HTTP_MOVED_PERM) {
+            String location = con.getHeaderField("Location");
+            URL newUrl = new URL(location);
+            con = (HttpURLConnection) newUrl.openConnection();
+        }
+
+        // Reading responses
+        status = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        // In the case that the request fails...
+        Reader streamReader = null;
+
+        // Get different stream depending if there's an error.
+        if (status > 299) {
+            streamReader = new InputStreamReader(con.getErrorStream());
+        } else {
+            streamReader = new InputStreamReader(con.getInputStream());
+        }
+        // Then we can carry on with the reading afterwards
+
+
+
         // Set up output stream
         DataOutputStream out = new DataOutputStream(con.getOutputStream());
         out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
         out.flush();
         out.close();
+
 
         System.out.println(ParameterStringBuilder.getParamsString(parameters));
     }
---
+++ src/FullResponseBuilder.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.util.Iterator;
+import java.util.List;
+
+public class FullResponseBuilder {
+    public static String getFullResponse(HttpURLConnection con) throws IOException {
+        // Building full response
+        // Adding the response status info
+        StringBuilder fullResponseBuilder = new StringBuilder();
+        fullResponseBuilder.append(con.getResponseCode())
+                .append(" ")
+                .append(con.getResponseMessage())
+                .append("\n");
+
+        // Getting the headers and adding each of them to the StringBuilder
+        // Done in the format HeaderName: HeaderValues
+        con.getHeaderFields().entrySet().stream()
+                .filter(entry -> entry.getKey() != null)
+                .forEach(entry -> {
+                    fullResponseBuilder.append(entry.getKey()).append(": ");
+                    List headerValues = entry.getValue();
+                    Iterator it = headerValues.iterator();
+                    if (it.hasNext()) {
+                        fullResponseBuilder.append(it.next());
+                        while (it.hasNext()) {
+                            fullResponseBuilder.append(", ").append(it.next());
+                        }
+                    }
+                    fullResponseBuilder.append("\n");
+                });
+        int status = con.getResponseCode();
+        Reader streamReader = null;
+
+        if (status > 299) {
+            streamReader = new InputStreamReader(con.getErrorStream());
+        } else {
+            streamReader = new InputStreamReader(con.getInputStream());
+        }
+
+        BufferedReader in = new BufferedReader(streamReader);
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            fullResponseBuilder.append(inputLine);
+        }
+        in.close();
+
+        return fullResponseBuilder.toString();
+    }
+}
